1d53daee703a2876beadd86bc5993db2b2aca049
Jeff Montoya <93927253+jeffmoontoya@users.noreply.github.com>
2023-06-21 11:31:16 -0500

    views controllers routes v.1

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
+    server: { hmr: { host: "localhost" } },
+
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
         vue({
@@ -22,7 +22,9 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+            vue: "vue/dist/vue.esm-bundler.js",
+            "@": path.resolve(__dirname, "resources/js"),
+            '~': path.resolve(__dirname, 'node_modules')
         },
     },
 });
